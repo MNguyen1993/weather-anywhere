c72be1154ccfe16943258fc98d808054553917d8
Minh Nguyen <M.Nguyen1993@yahoo.com>
2019-09-22 09:08:26 -0600

    Added font awesome icons and adjusted some info on about page

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Routes
 app.get('/', (req, res) => {
 	res.render('index', {
-		title: 'Weather App',
+		title: 'Weather Anywhere',
 		name: 'Minh Nguyen'
 	});
 });
